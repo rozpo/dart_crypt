3ebca91ca6a6b671074f36adedbfd5b0be03986d
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-07-17 14:12:58 +0100

    Move CommandRunner logic to separated file

--- bin/dcrypt.dart
+++ bin/dcrypt.dart
@@ -1,58 +1,7 @@
-import 'dart:io';
-
-import 'package:args/command_runner.dart';
-import 'package:dart_crypt/commands/decrypt.dart';
-import 'package:dart_crypt/commands/encrypt.dart';
-import 'package:dart_crypt/config/strings.dart';
+import 'package:dart_crypt/dart_crypt.dart';
 
 void main(List<String> arguments) {
-  final runner = CommandRunner(
-    Strings.binName,
-    Strings.binDesc,
-  );
-
-  runner.argParser.addOption(
-    Strings.keyName,
-    abbr: Strings.keyName[0],
-    help: Strings.keyDesc,
-    valueHelp: Strings.keyValue,
-  );
-
-  runner.argParser.addOption(
-    Strings.inputName,
-    abbr: Strings.inputName[0],
-    help: Strings.inputDesc,
-    valueHelp: Strings.inputValue,
-  );
-
-  runner.argParser.addOption(
-    Strings.outputName,
-    abbr: Strings.outputName[0],
-    help: Strings.outputDesc,
-    valueHelp: Strings.outputValue,
-  );
-
-  runner.argParser.addOption(
-    Strings.verboseName,
-    abbr: Strings.verboseName[0],
-    help: Strings.verboseDesc,
-  );
-
-   runner.argParser.addOption(
-    Strings.versionName,
-    help: Strings.versionDesc,
-  );
-
-  final args = runner.parse(arguments);
-
-  runner.addCommand(EncryptCommand(args));
-  runner.addCommand(DecryptCommand(args));
-
-  runner.run(arguments).catchError(
-    (error) {
-      if (error is! UsageException) throw error;
-      print(error);
-      exit(64);
-    },
-  );
+  DartCrypt.init();
+  DartCrypt.parse(arguments);
+  DartCrypt.run(arguments);
 }
---
+++ lib/dart_crypt.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+
+import 'commands/decrypt.dart';
+import 'commands/encrypt.dart';
+import 'config/strings.dart';
+
+mixin DartCrypt {
+  static CommandRunner runner = CommandRunner(
+    Strings.binName,
+    Strings.binDesc,
+  );
+
+  static late ArgResults args;
+
+  static void init() {
+    runner.argParser.addOption(
+      Strings.keyName,
+      abbr: Strings.keyName[0],
+      help: Strings.keyDesc,
+      valueHelp: Strings.keyValue,
+    );
+
+    runner.argParser.addOption(
+      Strings.inputName,
+      abbr: Strings.inputName[0],
+      help: Strings.inputDesc,
+      valueHelp: Strings.inputValue,
+    );
+
+    runner.argParser.addOption(
+      Strings.outputName,
+      abbr: Strings.outputName[0],
+      help: Strings.outputDesc,
+      valueHelp: Strings.outputValue,
+    );
+
+    runner.argParser.addOption(
+      Strings.verboseName,
+      abbr: Strings.verboseName[0],
+      help: Strings.verboseDesc,
+    );
+
+    runner.argParser.addOption(
+      Strings.versionName,
+      help: Strings.versionDesc,
+    );
+  }
+
+  static void parse(List<String> arguments) {
+    args = runner.parse(arguments);
+  }
+
+  static void run(List<String> arguments) {
+    runner.addCommand(EncryptCommand(args));
+    runner.addCommand(DecryptCommand(args));
+
+    runner.run(arguments).catchError(
+      (error) {
+        if (error is! UsageException) throw error;
+        print(error);
+        exit(64);
+      },
+    );
+  }
+}
