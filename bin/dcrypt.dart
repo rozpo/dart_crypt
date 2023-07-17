b6fb9528c9fe44279f86b867d8d0d8ae052b549b
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-07-17 14:03:24 +0100

    Add basic global options for further use

--- bin/dcrypt.dart
+++ bin/dcrypt.dart
@@ -11,6 +11,38 @@ void main(List<String> arguments) {
     Strings.binDesc,
   );
 
+  runner.argParser.addOption(
+    Strings.keyName,
+    abbr: Strings.keyName[0],
+    help: Strings.keyDesc,
+    valueHelp: Strings.keyValue,
+  );
+
+  runner.argParser.addOption(
+    Strings.inputName,
+    abbr: Strings.inputName[0],
+    help: Strings.inputDesc,
+    valueHelp: Strings.inputValue,
+  );
+
+  runner.argParser.addOption(
+    Strings.outputName,
+    abbr: Strings.outputName[0],
+    help: Strings.outputDesc,
+    valueHelp: Strings.outputValue,
+  );
+
+  runner.argParser.addOption(
+    Strings.verboseName,
+    abbr: Strings.verboseName[0],
+    help: Strings.verboseDesc,
+  );
+
+   runner.argParser.addOption(
+    Strings.versionName,
+    help: Strings.versionDesc,
+  );
+
   final args = runner.parse(arguments);
 
   runner.addCommand(EncryptCommand(args));
--- lib/config/strings.dart
+++ lib/config/strings.dart
@@ -1,15 +1,41 @@
 mixin Strings {
   static const String binName = 'dcrypt';
   static const String binDesc =
-      'Command-line interface application that provides encryption and decryption capabilities for your sensitive data';
+      'Command-line interface application that provides encryption and decryption capabilities for your sensitive data.';
 
   // encrypt command
   static const String encryptName = 'encrypt';
   static const String encryptDesc =
-      'Encrypt your sensitive data using a specified encryption algorithm and a secret key';
+      'Encrypt your sensitive data using a specified encryption algorithm and a secret key.';
 
   // decrypt command
   static const String decryptName = 'decrypt';
   static const String decryptDesc =
-      'Decrypt previously encrypted data using the same encryption algorithm and secret key';
+      'Decrypt previously encrypted data using the same encryption algorithm and secret key.';
+
+  // key option
+  static const String keyName = 'key';
+  static const String keyDesc =
+      'Provide the secret key used during encryption.';
+  static const String keyValue = 'key';
+
+  // input option
+  static const String inputName = 'input';
+  static const String inputDesc =
+      'Specify the path to the encrypted file or enter the encrypted text.';
+  static const String inputValue = 'data';
+
+  // output option
+  static const String outputName = 'output';
+  static const String outputDesc =
+      'Specify the output file path to store the decrypted data.';
+  static const String outputValue = 'path';
+
+  // verbose option
+  static const String verboseName = 'verbose';
+  static const String verboseDesc = 'Show additional command output.';
+
+  // version option
+  static const String versionName = 'version';
+  static const String versionDesc = 'Print the dcrypt version.';
 }
