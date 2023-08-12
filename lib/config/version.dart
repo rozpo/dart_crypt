148b5d31e20c3423173d03f9827757fe63e2ef7a
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-12 16:09:14 +0100

    TODO automatic version control system note

--- lib/config/version.dart
+++ lib/config/version.dart
@@ -1,3 +1,5 @@
+// TODO Implement automatic version control system
+// While bumping the version validate integrity with pubspec.yaml file.
 mixin Version {
   static const String number = '0.4.1';
   static const String name = 'v$number';
