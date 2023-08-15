3dde117cf045742fe1d137e1380f5f281ac1cc97
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-15 22:32:30 +0100

    Prepare basic keygen command implementation

--- bin/src/commands/utils/keygen/keygen.dart
+++ bin/src/commands/utils/keygen/keygen.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+import 'dart:typed_data';
+
 import '../utils.dart';
 
 class KeygenCommand extends UtilsCommand {
@@ -5,5 +10,16 @@ class KeygenCommand extends UtilsCommand {
   String get name => 'keygen';
 
   @override
-  String get description => 'Generate random key for encryption';
+  String get description => 'Generate random key for encryption.';
+
+  @override
+  FutureOr? run() {
+    print(
+      base64.encode(
+        Uint8List.fromList(
+          List.generate(32, (index) => Random().nextInt(256)),
+        ),
+      ),
+    );
+  }
 }
