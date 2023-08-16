918c740f0240242a0459127ea4c20d2e7c41a9b2
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-16 11:44:48 +0100

    Self update from local while in root directory

--- bin/src/commands/utils/update/update.dart
+++ bin/src/commands/utils/update/update.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import '../../../utils/logger.dart';
 import '../../../utils/strings.dart';
 import '../utils.dart';
 
@@ -7,4 +11,16 @@ class UpdateCommand extends UtilsCommand {
 
   @override
   String get description => 'Update ${Strings.binName} to the latest version';
+
+  @override
+  FutureOr? run() {
+    var cmd = Process.runSync(
+        'dart', ['pub', 'global', 'activate', '--source', 'path', '.']);
+
+    if (cmd.exitCode == 0) {
+      Logger.info.log(cmd.stdout);
+    } else {
+      Logger.error.log(cmd.stderr);
+    }
+  }
 }
